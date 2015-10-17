6079f2fe1d00fc3777c5862efee2fbbc7087d0c7
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-10-17 14:21:08 -0700

    Building editor settings panel

--- public/javascripts/controllers/sidebar.js
+++ public/javascripts/controllers/sidebar.js
@@ -1,6 +1,16 @@
 angular.module('readyou.sidebar', [])
   .controller('TabCtrl', function TabCtrl() {
+    this.settings = true;
     this.toggleTab = function (tabName) {
       this[tabName] = !this[tabName];
     };
-  });
+  })
+
+  .controller('EditorSettingsCtrl', ['$scope',
+    function EditorSettingsCtrl($scope) {
+      $scope.opts = {line_wrap: true, tab_size: 2, soft_tabs: true};
+      $scope.$watchCollection('opts', function () {
+        console.log($scope.opts);
+      });
+    }
+  ]);
